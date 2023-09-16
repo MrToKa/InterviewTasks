8f907a1f8c8ff63c85bcf78df08de448ecc15d5b
TOKA <TOKA@DESKTOP-UG0HBL0>
2023-09-16 12:56:54 +0300

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static string ShowMenu()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Enter 'A' for addition.");
+            sb.AppendLine("Enter 'S' for subtraction.");
+            sb.AppendLine("Enter 'M' for multiplication.");
+            sb.AppendLine("Enter 'D' for division.");
+            sb.AppendLine("Enter 'Q' to quit the program.");
+
+            return sb.ToString();
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double ReadNumber(string input)
+        {
+            double num;
+            while (true)
+            {
+                Console.WriteLine(input);
+                if (double.TryParse(Console.ReadLine(), out num))
+                {
+                    return num;
+                }
+
+                Console.WriteLine("Invalid number. Please try again.");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(ShowMenu());
+
+            while (true)
+            {
+                char command = Console.ReadLine()[0];
+
+                if (command == 'Q')
+                {
+                    break;
+                }
+
+                double a;
+                double b;
+
+                switch (command)
+                {
+                    case 'A':
+                        a = ReadNumber("Enter the first number: ");
+                        b = ReadNumber("Enter the second number: ");
+                        Console.WriteLine($"The result is: {Add(a, b)}");
+                        break;
+                    case 'S':
+                        a = ReadNumber("Enter the first number: ");
+                        b = ReadNumber("Enter the second number: ");
+                        Console.WriteLine($"The result is: {Subtract(a, b)}");
+                        break;
+                    case 'M':
+                        a = ReadNumber("Enter the first number: ");
+                        b = ReadNumber("Enter the second number: ");
+                        Console.WriteLine($"The result is: {Multiply(a, b)}");
+                        break;
+                    case 'D':
+                        a = ReadNumber("Enter the first number: ");
+                        b = ReadNumber("Enter the second number: ");
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Cannot divide by zero.");
+                            break;
+                        }
+                        Console.WriteLine($"The result is: {Divide(a, b)}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command.");
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine(ShowMenu());
+            }
+        }
+    }
+}
---
+++ StudentsManagementSystem/Program.cs
@@ -0,0 +1,162 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StudentsManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                Console.WriteLine(Menu());
+
+                string input = Console.ReadLine();
+
+                int rollNum;
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Please enter Name, Roll number and GPA");
+                        string name = ReadName();
+                        rollNum = ReadRollNumber("Enter a Roll number: ");
+
+                        if (RollNumExist(rollNum, students))
+                        {
+                            Console.WriteLine("Duplicated roll number. Please enter a different Roll number.");
+                            rollNum = ReadRollNumber("Enter a Roll number: ");
+                        }
+
+                        double gpa = ReadGPA("Enter a GPA: ");
+
+                        Student student = new Student(name, rollNum, gpa);
+
+                        Console.WriteLine($"Student {student.Name} with roll number {student.RollNumber} and GPA {student.GPA:F2} was added to the system.");
+
+                        students.Add(student);
+                        break;
+                    case "2":
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("Students in class:");
+                        foreach (var participantStudent in students)
+                        {
+                            sb.AppendLine($"{participantStudent.Name} with roll number {participantStudent.RollNumber} has GPA {participantStudent.GPA:F2}");
+                        }
+                        Console.WriteLine(sb.ToString());
+                        break;
+                    case "3":
+                        rollNum = ReadRollNumber("Please enter a Roll number: ");
+                        Student searchedStudent = students.Find(x => x.RollNumber == rollNum);
+                        if (searchedStudent != null)
+                        {
+                            Console.WriteLine($"{searchedStudent.Name} with roll number {searchedStudent.RollNumber} has GPA {searchedStudent.GPA:F2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"No student with Roll number {rollNum} was found!");
+                        }
+                        break;
+                    case "4":
+                        double averageGrade = students.Average(x => x.GPA);
+                        Console.WriteLine($"Average GPA of the class is {averageGrade:F2}");
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input! Please choose a number from 1 to 5");
+                        break;
+                }
+            }
+        }
+
+        private static bool RollNumExist(int rollNum, List<Student> studentsList)
+        {
+            return studentsList.Exists(x => x.RollNumber == rollNum);
+        }
+
+        public static string ReadName()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a name: ");
+                string name = Console.ReadLine();
+                Regex regex = new Regex(@"^([a-zA-Z]+[_\s-]*)+$");
+
+                if (!regex.IsMatch(name))
+                {
+                    name = "";
+                }
+
+                if (!string.IsNullOrEmpty(name))
+                {
+                    return name;
+                }
+
+                Console.WriteLine("Invalid name. Please enter a valid name.");
+            }
+        }
+        public static int ReadRollNumber(string input)
+        {
+            while (true)
+            {
+                Console.WriteLine(input);
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    if (num >= 1) return num;
+                }
+
+                Console.WriteLine("Invalid Roll number. Please enter a valid Roll number.");
+            }
+        }        
+        
+        public static double ReadGPA(string input)
+        {
+            while (true)
+            {
+                Console.WriteLine(input);
+                if (double.TryParse(Console.ReadLine(), out double num))
+                {
+                    if (num is >= 0.0 and <= 100.0) return num;
+                }
+
+                Console.WriteLine("Invalid grade. Please enter a valid GPA score.");
+            }
+        }
+
+        public static string Menu()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Choose an option:");
+            sb.AppendLine("1 - Add Student");
+            sb.AppendLine("2 - View Students");
+            sb.AppendLine("3 - Search Student by Roll Number");
+            sb.AppendLine("4 - Calculate Average GPA");
+            sb.AppendLine("5 - Exit");
+            return sb.ToString();
+        }
+    }
+
+    public class Student
+    {
+        public Student()
+        {
+            
+        }
+
+        public Student(string name, int rollNum, double gpa)
+        {
+            Name = name;
+            RollNumber = rollNum;
+            GPA = gpa;
+        }
+
+        public string Name { get; set; }
+
+        public int RollNumber { get; set; }
+
+        public double GPA { get; set; }
+    }
+}
